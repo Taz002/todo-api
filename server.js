506fa527cc96431de5fbbb00e1ee08e21e5a2db9
Arron Marshall <marshall.arron@gmail.com>
2015-11-18 06:40:55 -0800

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -57,15 +58,36 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
     
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
+    // call create on db.todo
+    // respond to api with 200 and todo
+    // res.status(400).json(e);
     
-    body.description = body.description.trim();
-    body.id = todoNextId++;
+//    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+//        return res.status(400).send();
+//    }
     
-    todos.push(body);
-    res.json(body);
+    db.todo.create(body).then(function (todo) {
+        res.json(todo.toJSON());
+    }, function (e) {
+        console.log('first error fired');
+        res.status(400).json(e);
+    }).catch(function (e) {
+        console.log('second error fired');
+        res.status(400).json(e);
+    });
+    
+    
+//    Todo.create({
+//        description: 'Walk my dog',
+//        completed: false
+//    })
+    
+//    
+//    body.description = body.description.trim();
+//    body.id = todoNextId++;
+//    
+//    todos.push(body);
+//    res.json(body);
 });
 
 // DELETE /todos/:id
@@ -109,6 +131,10 @@ app.put('/todos/:id', function (req, res) {
     
 });
 
-app.listen(PORT, function () {
-    console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function () {
+    console.log('Everything is synced');
+    app.listen(PORT, function () {
+        console.log('Express listening on port ' + PORT + '!');
+    });
 });
+
